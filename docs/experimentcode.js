9f35e0f86e31b8030619a07e4a469342079add9a
Matthew Loder <i.loder.matthew@gmail.com>
2026-04-06 14:22:27 -0400

    add support for data collection

--- docs/experimentcode.js
+++ docs/experimentcode.js
@@ -15,6 +15,16 @@ var jsPsych = initJsPsych({
     }
 });
 
+var subject_id = jsPsych.data.getURLVariable('PROLIFIC_PID');
+var study_id = jsPsych.data.getURLVariable('STUDY_ID');
+var session_id = jsPsych.data.getURLVariable('SESSION_ID');
+
+jsPsych.data.addProperties({
+    subject_id: subject_id,
+    study_id: study_id,
+    session_id: session_id
+  });
+
 function loadCSV(filepath) {
   return new Promise((resolve, reject) => {
     Papa.parse(filepath, {
